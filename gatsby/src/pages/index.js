d8bf8eaad0eee7e34041d62385ac5d38a3e05936
Hudson, David <david.hudson@zywave.com>
2022-01-03 11:47:07 -0600

    temp: think I found the prob maybe w form

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,20 +1,19 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
-import Layout from '../components/Layout';
 
 export default function HomePage() {
   return (
-    <Layout title="welcome">
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </Layout>
+    <form name="Contact Form" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="Contact Form" />
+      <div>
+        <label>Your Email:</label>
+        <input type="email" name="email" />
+      </div>
+      <div>
+        <label>Message:</label>
+        <textarea name="message" />
+      </div>
+      <button type="submit">Send</button>
+    </form>
   );
 }
